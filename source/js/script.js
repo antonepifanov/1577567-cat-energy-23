639d702d24d6a47cb74f53a35427084edfe29db6
antonepifanov <antonepifanov87@gmail.com>
2021-08-30 21:21:38 +0300

    Оптимизация

--- source/js/script.js
+++ source/js/script.js
@@ -1,41 +1,35 @@
-const navMain = document.querySelector('.menu')
-const navToggle = document.querySelector('.menu__button')
+const navMain = document.querySelector(".menu")
+const navToggle = document.querySelector(".menu__button")
 
-navMain.classList.remove('menu--nojs')
+navMain.classList.remove("menu--nojs")
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('menu--closed')) {
-    navMain.classList.remove('menu--closed')
-    navMain.classList.add('menu--opened')
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("menu--closed")) {
+    navMain.classList.remove("menu--closed")
+    navMain.classList.add("menu--opened")
   } else {
-    navMain.classList.add('menu--closed')
-    navMain.classList.remove('menu--opened')
+    navMain.classList.add("menu--closed")
+    navMain.classList.remove("menu--opened")
   }
 })
 
 ymaps.ready(function () {
-  var myMap = new ymaps.Map('map', {
-          center: [59.938635, 30.323118],
-          zoom: 17
-      }, {
-          searchControlProvider: 'yandex#search'
-      }),
-      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-          hintContent: '',
-          balloonContent: 'CatEnergy'
-      }, {
-          // Опции.
-          // Необходимо указать данный тип макета.
-          iconLayout: 'default#image',
-          // Своё изображение иконки метки.
-          iconImageHref: '../img/map-pin.png',
-          // Размеры метки.
-          iconImageSize: [57, 53],
-          // Смещение левого верхнего угла иконки относительно
-          // её "ножки" (точки привязки).
-          iconImageOffset: [-15, -38]
-      });
+  var myMap = new ymaps.Map("map", {center: [59.938635, 30.323118], zoom: 17}, {searchControlProvider: "yandex#search"}),
+  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: "",
+      balloonContent: "CatEnergy"
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: "default#image",
+      // Своё изображение иконки метки.
+      iconImageHref: "../img/map-pin.png",
+      // Размеры метки.
+      iconImageSize: [57, 53],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-15, -38]
+    });
   myMap.geoObjects.add(myPlacemark)
-  myMap.behaviors.disable('scrollZoom')
-});
-
+  myMap.behaviors.disable("scrollZoom")
+})
