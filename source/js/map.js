f2280a4c2ee592dbf00d9942daa686738037b300
antonepifanov <antonepifanov87@gmail.com>
2021-08-30 13:48:42 +0300

    ПП

--- source/js/map.js
+++ source/js/map.js
@@ -1,18 +1,3 @@
-const navMain = document.querySelector('.menu')
-const navToggle = document.querySelector('.menu__button')
-
-navMain.classList.remove('menu--nojs')
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('menu--closed')) {
-    navMain.classList.remove('menu--closed')
-    navMain.classList.add('menu--opened')
-  } else {
-    navMain.classList.add('menu--closed')
-    navMain.classList.remove('menu--opened')
-  }
-})
-
 ymaps.ready(function () {
   var myMap = new ymaps.Map('map', {
           center: [59.938635, 30.323118],
@@ -21,8 +6,6 @@ ymaps.ready(function () {
           searchControlProvider: 'yandex#search'
       }),
       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-          hintContent: '',
-          balloonContent: 'CatEnergy'
       }, {
           // Опции.
           // Необходимо указать данный тип макета.
